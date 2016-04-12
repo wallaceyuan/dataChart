dca43b81f4661c8e9f78b060278ab1db31494fb7
wallaceyuan <1272864289@qq.com>
2016-04-12 10:11:16 +0800

    site

--- middleware/index.js
+++ middleware/index.js
@@ -1,17 +1,18 @@
 var url = require('url');
 var db = require('../db/data');
+
 exports.checkLogin = function(req, res, next) {
     var url_parts = url.parse(req.url, true);
     var query = url_parts.query.id;
-    console.log(req.session.user);
     if (req.session.user) {
         console.log('已登');
         next();
     }else{
         if(query!=undefined){
             db.findUser(query,function(data){
-                if(data.length){
-                    var user = JSON.parse(data)[0].code;
+                var result = JSON.parse(data);
+                if(result.length>0){
+                    var user = result[0].code;
                     console.log('登录成功',user);
                     req.session.user = user;
                 }else{
@@ -20,6 +21,7 @@ exports.checkLogin = function(req, res, next) {
                 next();
             });
         }else{
+            console.log('快去登录');
             next();
         }
     }
