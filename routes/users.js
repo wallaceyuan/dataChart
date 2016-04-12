15b9f04c4795cfb6fc275208f1bba51f1a35b2c2
wallaceyuan <1272864289@qq.com>
2016-04-12 18:12:11 +0800

    loginbug

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,30 @@ var express = require('express');
 var db = require('../db/data');
 var router = express.Router();
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
     res.render('user', { title: 'Express' });
 });
-router.post('/info',function(req, res, next) {
+
+
+router.post('/login',function(req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    var query = req.body.openId;
+    db.findUser(query,function(data){
+        var result = JSON.parse(data);
+        if(result.length>0){
+            var user = result[0].code;
+            console.log('登录成功',user);
+            req.session.user = user;
+            return res.redirect('/');
+        }else{
+            console.log('登录失败');
+            res.send({code:302});
+        }
+    });
+});
+
+
+router.post('/reg',function(req, res) {
     res.header("Access-Control-Allow-Origin", "*");
     var query = req.body;
     db.regUser(query,function(data){
@@ -14,4 +34,6 @@ router.post('/info',function(req, res, next) {
         res.end('123');
     });
 });
+
+
 module.exports = router;
